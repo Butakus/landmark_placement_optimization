bb95d9222267e0c705d856a831dc4822b17639e0
Francisco Miguel Moreno <butakus@gmail.com>
2023-07-17 10:39:28 +0200

    Avoid recomputing heatmap in plot_tool.py

--- lpo/plot_tool.py
+++ lpo/plot_tool.py
@@ -13,9 +13,10 @@ from heatmap import Heatmap
 # Set numpy random seed
 np.random.seed(42)
 
-LANDMARKS_FILE = "landmarks.npy"
+LANDMARKS_FILE_DEFAULT = "landmarks.npy"
+HEATMAP_FILE_DEFAULT = "heatmap.npy"
 NLLS_SAMPLES = 500
-N_THREADS = 8
+N_THREADS = 4
 
 def main(args):
     map_data = pgm.read_pgm(args.map_file)
@@ -30,7 +31,7 @@ def main(args):
     # Find a landmark setup that guarantees the desired accuracy
     lpo = LPO(map_data, args.map_resolution)
 
-    landmarks = np.load(LANDMARKS_FILE)
+    landmarks = np.load(args.landmarks)
     # landmarks = np.array([
     #     [5.0, 40.0, 0.0],
     #     [40.0, 50.0, 0.0],
@@ -116,11 +117,20 @@ def main(args):
         print("Cleaning up the solution...")
         landmarks = lpo.clean_solution(landmarks)
 
-    heatmap_builder = Heatmap(map_data, args.map_resolution,
-                              nlls_samples=NLLS_SAMPLES,
-                              n_threads=N_THREADS,
-                              progress=True)
-    heatmap = heatmap_builder.compute_heatmap(landmarks, 'nlls')
+    heatmap = None
+    if args.heatmap_in:
+        # Heatmap already computed, just load it
+        heatmap = np.load(args.heatmap_in)
+    else:
+        # Must compute the accuracy heatmap
+        heatmap_builder = Heatmap(map_data, args.map_resolution,
+                                nlls_samples=NLLS_SAMPLES,
+                                n_threads=N_THREADS,
+                                progress=True)
+        heatmap = heatmap_builder.compute_heatmap(landmarks, 'nlls')
+        # Save heatmap to file
+        np.save(args.heatmap_out, heatmap)
+    # Specific heatmap analysis for parking_lot case. SSomething weird with rows 3 and 4
     heatmap_mean_old = np.mean(heatmap)
     heatmap_max_old = np.max(heatmap)
     for i in range(heatmap.shape[0]):
@@ -135,7 +145,6 @@ def main(args):
     print("heatmap max: {}".format(np.max(heatmap)))
     if np.max(heatmap) > 0.0:
         print("heatmap average: {}".format(np.average(heatmap, weights=(heatmap > 0))))
-    # heatmap = None
 
     coverage = lpo.get_coverage_map(landmarks)
     print(coverage)
@@ -157,14 +166,19 @@ if __name__ == '__main__':
     parser.add_argument('map_resolution', metavar='map_resolution', type=float,
                         help='Map resolution (m/cell)')
     parser.add_argument('-l', '--landmarks', metavar='landmarks_file', type=str,
+                        default=LANDMARKS_FILE_DEFAULT,
                         help='Path to file to save best landmarks (.npy)')
+    parser.add_argument('--heatmap-in', metavar='heatmap_in_file', type=str,
+                        help='Path to file with previously computed heatmap')
+    parser.add_argument('--heatmap-out', metavar='heatmap_out_file', type=str,
+                        default=HEATMAP_FILE_DEFAULT,
+                        help='Path to file to save computed heatmap')
     parser.add_argument('-c', '--cleanup', action='store_true',
                         help='Cleanup solution before plotting (slow)')
     args = parser.parse_args()
 
-    # Change landmarks file if needed and make path absolute
-    if args.landmarks and not os.path.isdir(args.landmarks):
-        LANDMARKS_FILE = args.landmarks
-    LANDMARKS_FILE = os.path.abspath(LANDMARKS_FILE)
+    # Make paths absolute
+    args.landmarks = os.path.abspath(args.landmarks)
+    args.heatmap_out = os.path.abspath(args.heatmap_out)
 
     main(args)
